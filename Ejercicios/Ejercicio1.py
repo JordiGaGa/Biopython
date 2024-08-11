c9e4d7c618a98b5f8edf5e08296b6d7f5c5a47a9
JordiGaGa <jordigg@lcg.unam.mx>
2024-08-11 11:57:29 -0600

    Comentarios agregados

--- Ejercicios/Ejercicio1.py
+++ Ejercicios/Ejercicio1.py
@@ -10,13 +10,13 @@ from pprint import pprint  # para mejor visualización de diccionarios!!
 # ===========================================================================
 
 Entrez.email = "jordigg@lcg.unam.mx" # Correo
-handle = Entrez.einfo(db = "pubmed") # indicar db de interes
-record = Entrez.read(handle)
-"""
+handle = Entrez.einfo(db = "pubmed") # indicar db (base de datos) de interes
+record = Entrez.read(handle) #Se lee la base de datos 
+
+# Imprimir cada Name, FullName y Description de los elementos de la base de datos
 for field in record["DbInfo"]["FieldList"]:
-  #print("%(Name)s, %(FullName)s, %(Description)s" % field)
   print(field["Name"], field["FullName"]+": "+field["Description"])
-"""
+
 # Imprimir sólo el primer elemento de la lista Fiedlist
 for field in record["DbInfo"]["FieldList"]:
   if field["Name"] == "ALL":
