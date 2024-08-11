ff0f7eb3e3613c80fa873afa7fe4d573cdc5ae59
JordiGaGa <jordigg@lcg.unam.mx>
2024-08-11 11:54:38 -0600

    Ejercicio 1 y reacomodo de carpetas

--- Ejercicios/Ejercicio1.py
+++ Ejercicios/Ejercicio1.py
@@ -12,6 +12,13 @@ from pprint import pprint  # para mejor visualización de diccionarios!!
 Entrez.email = "jordigg@lcg.unam.mx" # Correo
 handle = Entrez.einfo(db = "pubmed") # indicar db de interes
 record = Entrez.read(handle)
+"""
 for field in record["DbInfo"]["FieldList"]:
   #print("%(Name)s, %(FullName)s, %(Description)s" % field)
-  print(field["FullName"]+": "+field["Description"])
+  print(field["Name"], field["FullName"]+": "+field["Description"])
+"""
+# Imprimir sólo el primer elemento de la lista Fiedlist
+for field in record["DbInfo"]["FieldList"]:
+  if field["Name"] == "ALL":
+    print(field["Description"])
+    exit
