150e40a371d043e756c832aa524351077d6917d5
JordiGaGa <jordigg@lcg.unam.mx>
2024-08-26 22:26:00 -0600

    Tarea 3

---
+++ Ejercicios/Ejercicio3.py
@@ -0,0 +1,62 @@
+"""
+búsqueda de linajes. Busquemos qué tan emparentados están dos organismos con las herramientas que hemos visto.
+
+Usaremos a Notoryctes typhlops y Chrysochloris asiatica
+
+Como nuestra pregunta es sobre linajes, la base de datos que usaremos será Taxonomy.
+
+PRIMERA PARTE: Hacer una búsqueda con esearch (en este caso contamos con 
+#los nombres de los organismos), la búsqueda nos dará su ID.
+SEGUNDA PARTE: Usar el ID para obtener archivo con el linale de cada topo.
+Contesta la pregunta:  En que punto divergen sus linajes.
+Punto Opcional:  ¿como compararías ambas linajes para que la respuesta fuera automática?
+"""
+# ===========================================================================
+# =                            imports
+# ===========================================================================
+
+from Bio import Entrez
+
+# ===========================================================================
+# =                            Main
+# ===========================================================================
+Entrez.email = "jordigg@lcg.unam.mx" # Correo
+handle = Entrez.esearch(db = "Taxonomy", term="Notoryctes typhlops[Orgn] OR Chrysochloris asiatica[Orgn]") # indicar db (base de datos) de interes
+record = Entrez.read(handle)
+ids = record["IdList"]
+
+def Archivo_IDs(ids, filename): # Se crea archivo con IDs obtenidos de los 2 organismos
+    with open(filename, 'w') as f:
+        for id in ids:
+            f.write(f"{id}\n")
+
+filename = "IDs.txt"
+Archivo_IDs(ids, filename)
+
+with open(filename, 'r') as f:  # Se abre el archivo y se lee
+    org1 = 0
+    for num, id_taxo in enumerate(f):
+        handle2 = Entrez.efetch(db="Taxonomy", id=id_taxo, retmode="xml")
+        org2 = Entrez.read(handle2) # Se queda el segundo organismo
+        if num == 0:
+            org1 = org2[0]["Lineage"] # Se guarda el primer organismo
+
+        tax1 = org1.split(";")
+        tax2 = org2[0]["Lineage"].split(";") 
+        min_len = min(len(tax1), len(tax2)) # Se analiza la longitud en palabras de ambos para tomar la mínima para el ciclo for
+        for i in range(min_len): 
+            if tax1[i] != tax2[i]: 
+                print(f"Los organismos difieren en la categoría {i+1}: {tax1[i]} y {tax2[i]}")
+                exit()
+
+
+
+
+
+
+
+
+
+
+
+
